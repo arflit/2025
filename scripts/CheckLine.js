febee5df88e32f938dd17d623c04424df456752d
Arkady Flitman <arflit@gmail.com>
2020-12-20 11:48:59 +0300

    Обработка строк

--- scripts/CheckLine.js
+++ scripts/CheckLine.js
@@ -1,7 +1,8 @@
 export class CheckLine {
-  constructor (textLine, source) {
+  constructor (textLine, number, isGood) {
     this._line = textLine;
-    this._source = source;
+    this._number = number;
+    this._isGood = isGood;
   }
 
   _getTemplate() {
@@ -14,28 +15,22 @@ export class CheckLine {
     return row;
   }
 
-  _isGood() {
-    const number = Math.random();
-    const isGood = (number < 0.6);
-    return isGood;
-  }
-
   _addGoodRow() {
     this._row = this._getTemplate();
-    this._row.classList.add('checkresult__row_good');
-    this._row.querySelector('.checkresult__source').textContent = this._source;
-    this._row.querySelector('.checkresult__quote').textContent = this._line;
+    this._row.querySelector('.checkresult__line').classList.add('checkresult__line_good');
+    this._row.querySelector('.checkresult__number').textContent = this._number;
+    this._row.querySelector('.checkresult__line').textContent = this._line;
   }
 
   _addBadRow() {
     this._row = this._getTemplate();
-    this._row.classList.add('checkresult__row_bad');
-    this._row.querySelector('.checkresult__source').textContent = 'Классики такого не писали';
-    this._row.querySelector('.checkresult__quote').textContent = this._line;
+    this._row.querySelector('.checkresult__line').classList.add('checkresult__line_bad');
+    this._row.querySelector('.checkresult__number').textContent = this._number;
+    this._row.querySelector('.checkresult__line').textContent = this._line;
   }
 
   generateRow() {
-    if (this._isGood()) {
+    if (this._isGood) {
       this._addGoodRow();
     } else {
       this._addBadRow();
--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,16 @@
 const form = document.forms.petitionCheck;
 const petitionField = form.elements.petition;
 const resetButton = document.querySelector('#resetButton');
-const formSection = document.querySelector('.chcekform');
+const formSection = document.querySelector('.form');
+const about = document.querySelector('.about');
+const checkresult = document.querySelector('.checkresult');
+const analytics = document.querySelector('.analytics');
+const testButton = document.querySelector('.about__testbutton');
 const resultSection = document.querySelector('.checkresult');
-const rowContainer = resultSection.querySelector('.checkresult__container');
-import { FormValidator } from './FormValidator.js';
+const backButton = document.querySelector('.checkresult__backbutton');
+const rowContainer = resultSection.querySelector('.checkresult__rowcontainer');
 import { CheckLine } from './CheckLine.js';
-
+/* import { FormValidator } from './FormValidator.js';
 const validationSettings = {
   formSelector: '.form',
   inputSelector: '.form__input',
@@ -15,37 +19,73 @@ const validationSettings = {
   inputErrorClass: '',
   errorClass: 'form__error_visible'
 }
-const poets = ['А.С.Пушкин', 'М.Ю.Лермонтов', 'В.А.Жуковский', 'А.А.Блок', 'М.И.Цветаева', 'В.В.Маяковский'];
-
 const formValidation = new FormValidator(validationSettings, form);
-formValidation.enableValidation();
+formValidation.enableValidation(); */
 
-function addRow(textLine, source) {
-  const newRow = new CheckLine(textLine, source);
-  rowContainer.append(newRow.generateRow());
+const testText = `Безмолвное море, лазурное море,
+Стою очарован над бездной твоей.
+Ты живо; ты дышишь; смятенной любовью,
+Тревожною думой наполнено ты.
+Безмолвное море, лазурное море,
+Открой мне глубокую тайну твою.
+Что движет твое необъятное лоно?
+Чем дышит твоя напряженная грудь?
+Иль тянет тебя из земныя неволи
+Далекое, светлое небо к себе?..
+Таинственной, сладостной полное жизни,
+Ты чисто в присутствии чистом его:
+Ты льешься его светозарной лазурью,
+Вечерним и утренним светом горишь,
+Ласкаешь его облака золотые
+И радостно блещешь звездами его.
+Когда же сбираются темные тучи,
+Чтоб ясное небо отнять у тебя -
+Ты бьешься, ты воешь, ты волны подъемлешь,
+Ты рвешь и терзаешь враждебную мглу...
+И мгла исчезает, и тучи уходят,
+Но, полное прошлой тревоги своей,
+Ты долго вздымаешь испуганны волны,
+И сладостный блеск возвращенных небес
+Не вовсе тебе тишину возвращает;
+Обманчив твоей неподвижности вид:
+Ты в бездне покойной скрываешь смятенье,
+Ты, небом любуясь, дрожишь за него.`;
+
+function checkRow() {
+  const number = Math.random();
+  const isGood = (number < 0.7);
+  return isGood;
 }
 
-function getRandom(poets) {
-  const rand = Math.floor(Math.random() * poets.length);
-  return poets[rand];
+function addRow(textLine, number, isgood) {
+  const newRow = new CheckLine(textLine, number, isgood);
+  rowContainer.append(newRow.generateRow());
 }
 
 function showResult(rowList) {
   while (rowContainer.firstChild) {rowContainer.removeChild(rowContainer.firstChild);}
-  rowList.forEach(function(row) {
+  rowList.forEach(function(row, number) {
     if (row) {
-      addRow(row, getRandom(poets));
+      const isGood = checkRow();
+      addRow(row, number, isGood);
     }
   });
   formSection.classList.add('hidden');
-  resultSection.classList.remove('hidden');
+  about.classList.add('hidden');
+  checkresult.classList.remove('hidden');
+  analytics.classList.remove('hidden');
 }
 
 function showForm() {
-  form.reset();
   formSection.classList.remove('hidden');
-  resultSection.classList.add('hidden');
+  about.classList.remove('hidden');
+  checkresult.classList.add('hidden');
+  analytics.classList.add('hidden');
 }
+
+testButton.addEventListener('click', function() {
+  petitionField.value = testText;
+})
 
 form.addEventListener('submit', function(evt) {
   evt.preventDefault();
@@ -54,7 +94,9 @@ form.addEventListener('submit', function(evt) {
   showResult(rowList);
 });
 
+backButton.addEventListener('click', showForm);
 resetButton.addEventListener('click', function() {
+  form.reset();
   showForm();
 });
 
